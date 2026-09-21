72abd10295cf15882e8089903511eb511fe316a1
Kai Xin <tankaixin0212@gmail.com>
2026-09-21 14:31:58 +0800

    feat: display drop down of categories

--- client/src/views/MenuView.vue
+++ client/src/views/MenuView.vue
@@ -2,30 +2,34 @@
 import { ref, onMounted } from "vue";
 import ItemsBrowser from "../components/ItemsBrowser.vue";
 import axios from "axios";
-
-
 // Reactive data
 const categories = ref([])
 const selected_category = ref('fruit')
 const items = ref([])
 const cartItems = ref([]);
-
 // local storage
 const STORAGE_KEY = "cart"
-
 // TODO: Fetch categories when the component is created
-
-
+onMounted( async () => {
+    let url = "http://127.0.0.1:3000/categories"
+    try {
+        // axios code must be inside try/catch block
+        let response = await axios.get(url)
+        console.log(response.data)
+        categories.value = response.data
+    } catch(e) {
+        // to display the error message when there is a connection error
+        console.log(e.message)
+    }
+} ) 
 // TODO: Fetch items for the currently selected category
 async function getItems() {
     // Add code
 }
-
 // Add selected items to cart
 function doAddToCart(itemsToAdd) {
     for (const item of itemsToAdd) {
         let found = false;
-
         for (const cartItem of cartItems.value) {
             if (item.id == cartItem.id) {
                 found = true;
@@ -35,26 +39,21 @@ function doAddToCart(itemsToAdd) {
         }
         if (!found) cartItems.value.push(item);
     }
-
     // TODO: store current cartitems into local storage
     // cartItems.value is a JS (complex) obj. We need to use JSON.stringify to convert the JS obj to JSON string
    
     
 }
-
 </script>
-
 <template>
     <h1>Menu Items</h1>
   
     <!-- TODO: Category selection dropdown -->
     <label for="categories">Categories</label>
-    <select class="form-control" id="categories" >
-        <option> category </option>
+    <select class="form-control" id="categories" v-model="selected_category">
+        <option v-for="v in categories" > {{ v }} </option>
     </select>
     <br>
-
-
     <div class="container-fluid">
         <div class="row p-3">
             <div class='col-md-6 text-center'>
@@ -64,7 +63,6 @@ function doAddToCart(itemsToAdd) {
                 </button>
             </div>
         </div>
-
         <div class="row row-cols-auto justify-content-end">
             <RouterLink to="/cart">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
@@ -75,7 +73,17 @@ function doAddToCart(itemsToAdd) {
             </RouterLink>
         </div>
     </div>
-
 </template>
-
 <style scoped></style>
+Show less
+
+
+
+
+
+
+
+
+
+
+
